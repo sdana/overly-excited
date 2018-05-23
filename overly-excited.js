b8b0fe5e5dd5c092127f27a5ff8b677a19843f95
sdana <seth.dana@gmail.com>
2018-05-23 10:48:54 -0500

    rewrote function as arrow function

--- overly-excited.js
+++ overly-excited.js
@@ -1,13 +1,13 @@
 // Create an array that contains the words in the sentence
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
-let newSentence = ["this", "was", "a", "very", "interesting", "and", "fun", "challenge"];
+let newSentence = ["this", "was", "a", "very", "interesting", "and", "fun", "challenge", "and", "this", "is", "a", "very", "long", "sentence"];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray, theCharacter) {
+let addExcitement = (theWordArray, theCharacter) => {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
